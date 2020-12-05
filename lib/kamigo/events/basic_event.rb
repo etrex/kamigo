3e5a451ff4fd7d0836742646c67e6053b4b85036
chaochaowu <chaowu.dev@gmail.com>
2020-12-05 16:56:07 +0800

    Feature: add profile info into platform params description: platform params should include user profile info modify:   - add profile into platform_params   - line event parser add source profile from client

--- lib/kamigo/events/basic_event.rb
+++ lib/kamigo/events/basic_event.rb
@@ -7,6 +7,7 @@ module Kamigo
       attr_accessor :source_type
       attr_accessor :source_group_id
       attr_accessor :source_user_id
+      attr_accessor :source_profile
       attr_accessor :payload
 
       def platform_params
@@ -14,7 +15,8 @@ module Kamigo
           platform_type: platform_type,
           source_type: source_type,
           source_group_id: source_group_id,
-          source_user_id: source_user_id
+          source_user_id: source_user_id,
+          profile: source_profile
         }
       end
     end
