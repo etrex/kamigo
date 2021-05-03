557c6aa36422891e4851062c769b7a9e1500aebb
etrex kuo <et284vu065k3@gmail.com>
2021-05-04 00:36:47 +0800

    get profile when context is in groups or rooms

--- lib/kamigo/event_parsers/line_event_parser.rb
+++ lib/kamigo/event_parsers/line_event_parser.rb
@@ -14,17 +14,34 @@ module Kamigo
       def parse(event)
         event_hash = JSON.parse(event.to_json, symbolize_names: true)
         payload = event_hash[:src] || event_hash
-        response = client.get_profile(payload[:source][:userId])
         line_event = Kamigo::Events::LineEvent.new
         line_event.payload = payload
         line_event.reply_token = event['replyToken']
-        line_event.profile = JSON.parse(response.body)
         line_event.source_type = payload.dig(:source, :type)
         line_event.source_group_id = payload.dig(:source, :groupId) || payload.dig(:source, :roomId) || payload.dig(:source, :userId)
         line_event.source_user_id = payload.dig(:source, :userId) || payload.dig(:source, :groupId) || payload.dig(:source, :roomId)
         line_event.message_type = payload.dig(:message, :type) || payload.dig(:type)
         line_event.message = payload.dig(:message, :text) || payload.dig(:postback, :data) || payload.dig(:message, :address) || line_event.message_type
+        line_event.profile = get_profile(line_event)
         line_event
+      end
+
+      def get_profile(line_event)
+        case line_event.source_type
+        when 'group'
+          response = client.get_group_member_profile(
+            line_event.source_group_id,
+            line_event.source_user_id
+          )
+        when 'room'
+          response = client.get_room_member_profile(
+            line_event.source_group_id,
+            line_event.source_user_id
+          )
+        else
+          response = client.get_profile(line_event.source_user_id)
+        end
+        JSON.parse(response.body)
       end
     end
   end
