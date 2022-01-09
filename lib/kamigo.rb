92aa244efe4dc04751949b2e8f564556ed0a3056
etrex kuo <et284vu065k3@gmail.com>
2022-01-09 15:54:23 +0800

    add config

--- lib/kamigo.rb
+++ lib/kamigo.rb
@@ -26,6 +26,32 @@ module Kamigo
   mattr_accessor :line_event_processors
   @@line_event_processors = [EventProcessors::RailsRouterProcessor.new]
 
+  # env
+  mattr_writer :line_messaging_api_channel_id
+  mattr_writer :line_messaging_api_channel_secret
+  mattr_writer :line_messaging_api_channel_access_token
+
+  def self.line_message_api_channel_id
+    @@line_message_api_channel_id = ENV["LINE_CHANNEL_ID"]
+  end
+
+  def self.line_message_api_channel_secret
+    @@line_message_api_channel_secret = ENV["LINE_CHANNEL_SECRET"]
+  end
+
+  def self.line_message_api_channel_token
+    @@line_message_api_channel_token = ENV["LINE_CHANNEL_TOKEN"]
+  end
+
+  class << self
+    delegate :line_login_channel_id, :line_login_channel_id=, to: :Kamiliff
+    delegate :line_login_channel_secret, :line_login_channel_secret=, to: :Kamiliff
+    delegate :line_login_redirect_uri, :line_login_redirect_uri=, to: :Kamiliff
+    delegate :liff_url_compact, :liff_url_compact=, to: :Kamiliff
+    delegate :liff_url_tall, :liff_url_tall=, to: :Kamiliff
+    delegate :liff_url_full, :liff_url_full=, to: :Kamiliff
+  end
+
   def self.setup
     yield self
   end
