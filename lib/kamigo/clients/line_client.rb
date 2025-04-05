63f92b78490dd539f959501e846d624c9ef5a490
kami <et284vu065k3@gmail.com>
2025-04-05 23:28:08 +0800

    Bump version to 0.35.0

--- lib/kamigo/clients/line_client.rb
+++ lib/kamigo/clients/line_client.rb
@@ -3,14 +3,21 @@ module Kamigo
     module LineClient
       def client
         @client ||= Line::Bot::Client.new do |config|
-          config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-          config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+          config.channel_id = Kamigo.line_messaging_api_channel_id
+          config.channel_secret = Kamigo.line_messaging_api_channel_secret
+          config.channel_token = Kamigo.line_messaging_api_channel_token
         end
       end
 
       def validate_signature(request, body)
         signature = request.env['HTTP_X_LINE_SIGNATURE']
         client.validate_signature(body, signature)
+      end
+
+      private
+
+      def reset_client
+        @client = nil
       end
     end
   end
